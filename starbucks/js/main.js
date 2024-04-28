9337299df03dc11b2fb7780f6c5e3e42c89f571b
TAEHONG-LEE <2t_hong@naver.com>
2024-04-29 00:21:04 +0900

    starbucks notice

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -9,7 +9,6 @@ searchInputEl.addEventListener('focus', function () {
     searchEl.classList.add('focused');
     searchInputEl.setAttribute('placeholder', '통합검색');
 });
-
 searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
@@ -22,7 +21,7 @@ const toTopEl = document.querySelector('#to-top');
 window.addEventListener('scroll', _.throttle(function () {
     console.log(window.scrollY);
     if (window.scrollY > 500) {
-        // 배지 숨기기
+        // 배지 숨기기asd
         // gsap.to(요소, 지속시간, 옵션);
         gsap.to(badgeEl, .6, {
             opacity: 0,
@@ -47,3 +46,29 @@ fadeEls.forEach(function (fadeEl, index) {
         opacity: 1
     })
 });
+
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});    
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    loop: true,
+    autoplay: {
+        delay: 5000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+
+});
+
+
